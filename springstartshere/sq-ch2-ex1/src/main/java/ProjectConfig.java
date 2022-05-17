c549a1a7098370f335a0ee2cac71f4f85258d66d
nanshaqundao <nanshax@hotmail.co.uk>
2022-05-17 22:31:43 +0100

    sq-ch2-ex1

---
+++ springstartshere/sq-ch2-ex1/src/main/java/ProjectConfig.java
@@ -0,0 +1,13 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    Parrot parrot(){
+        var p = new Parrot();
+        p.setName("Koko");
+        return p;
+    }
+}
