44a23f795cf2511923b49690999e1d6e12fd1228
nanshaqundao <nanshax@hotmail.co.uk>
2022-05-30 00:01:49 +0100

    chap7

---
+++ springstartshere/apps/ch7-ex1/src/test/java/com/example/ch7ex1/MainTests.java
@@ -0,0 +1,13 @@
+package com.example.ch7ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
