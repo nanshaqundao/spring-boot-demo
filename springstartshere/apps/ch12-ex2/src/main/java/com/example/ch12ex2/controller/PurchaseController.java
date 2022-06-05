c8ac62614b3f4fbbe11df5260f154393cf74c1d0
nanshaqundao <nanshax@hotmail.co.uk>
2022-06-05 21:15:14 +0100

    the null insert into id auto_increment is not an issue for real mysql db, this is found in ch12-ex2

---
+++ springstartshere/apps/ch12-ex2/src/main/java/com/example/ch12ex2/controller/PurchaseController.java
@@ -0,0 +1,30 @@
+package com.example.ch12ex2.controller;
+
+
+import com.example.ch12ex2.model.Purchase;
+import com.example.ch12ex2.repository.PurchaseRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/purchase")
+public class PurchaseController {
+
+    private final PurchaseRepository purchaseRepository;
+
+    public PurchaseController(PurchaseRepository purchaseRepository) {
+        this.purchaseRepository = purchaseRepository;
+    }
+
+    @PostMapping
+    public void storePurchase(@RequestBody Purchase purchase) {
+        purchaseRepository.storePurchase(purchase);
+    }
+
+    @GetMapping
+    public List<Purchase> findPurchases() {
+        return purchaseRepository.findAllPurchases();
+    }
+}
+
