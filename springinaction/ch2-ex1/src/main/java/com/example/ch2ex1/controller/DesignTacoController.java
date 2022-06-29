3373456977e00320f240a033556ee1ec6f275480
nanshaqundao <nanshax@hotmail.co.uk>
2022-06-29 10:53:57 +0100

    init on this machine

--- springinaction/ch2-ex1/src/main/java/com/example/ch2ex1/controller/DesignTacoController.java
+++ springinaction/ch2-ex1/src/main/java/com/example/ch2ex1/controller/DesignTacoController.java
@@ -7,11 +7,10 @@ import com.example.ch2ex1.model.TacoOrder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -44,20 +43,30 @@ public class DesignTacoController {
     }
 
     @ModelAttribute(name = "tacoOrder")
-    public TacoOrder order(){
+    public TacoOrder order() {
         return new TacoOrder();
     }
 
     @ModelAttribute(name = "taco")
-    public Taco taco(){
+    public Taco taco() {
         return new Taco();
     }
 
     @GetMapping
-    public String showDesignForm(){
+    public String showDesignForm() {
         return "design";
     }
 
+    @PostMapping
+    public String showDesgnForm(@Valid Taco taco, Errors errors,
+                                @ModelAttribute TacoOrder tacoOrder) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
+        tacoOrder.addTaco(taco);
+        log.info("Processing taco: {}", taco);
+        return "redirect:/orders/current";
+    }
 
     private Iterable<Ingredient> filterByType(
             List<Ingredient> ingredients, Type type
