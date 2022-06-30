09fb1607852e8f430fa9b0b87bb5305e9a27f2b5
nanshaqundao <nanshax@hotmail.co.uk>
2022-06-30 23:52:14 +0100

    spring in action - ch03 - db init 1st part done

--- springinaction/ch03/src/main/resources/schema.sql
+++ springinaction/ch03/src/main/resources/schema.sql
@@ -10,3 +10,29 @@ create table if not exists Taco_Order (
     cc_cvv varchar(3) not null,
     created_at timestamp not null
 );
+
+create table if not exists Taco(
+    id identity,
+    name varchar(50) not null,
+    taco_order bigint not null,
+    taco_order_key bigint not null,
+    created_at timestamp not null
+);
+
+create table if not exists Ingredient_Ref(
+    ingredient varchar(4) not null,
+    taco bigint not null,
+    taco_key bigint not null
+);
+
+create table if not exists Ingredient(
+    id varchar(4) primary key,
+    name varchar(25) not null,
+    type varchar(10) not null
+);
+
+alter table Taco
+    add foreign key (taco_order) references Taco_Order(id);
+
+alter table Ingredient_Ref
+    add foreign key (ingredient) references Ingredient(id);
