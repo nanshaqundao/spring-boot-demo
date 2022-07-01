09e67bca9ac0408a7c0871ccc9c634539bb96ab5
nanshaqundao <nanshax@hotmail.co.uk>
2022-07-01 17:45:37 +0100

    init

---
+++ springinaction/ch03springdatajdbc/src/main/java/com/example/ch03springdatajdbc/Ch03springdatajdbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.ch03springdatajdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch03springdatajdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch03springdatajdbcApplication.class, args);
+    }
+
+}
---
+++ springinaction/ch03springdatajdbc/src/test/java/com/example/ch03springdatajdbc/Ch03springdatajdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ch03springdatajdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch03springdatajdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
