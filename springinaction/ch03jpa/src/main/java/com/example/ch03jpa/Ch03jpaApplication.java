ce218dd4b2996315831fd96bab0e7cabecb20a98
nanshaqundao <nanshax@hotmail.co.uk>
2022-07-01 22:17:44 +0100

    spring in action - ch03 - jpa init

---
+++ springinaction/ch03jpa/src/main/java/com/example/ch03jpa/Ch03jpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ch03jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch03jpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch03jpaApplication.class, args);
+    }
+
+}
---
+++ springinaction/ch03jpa/src/test/java/com/example/ch03jpa/Ch03jpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ch03jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch03jpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
