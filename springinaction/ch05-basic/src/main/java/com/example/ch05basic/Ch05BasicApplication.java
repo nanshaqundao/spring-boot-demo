682fae2f678f6da8ca6b1a48c16871917ba76a63
nanshaqundao <nanshax@hotmail.co.uk>
2022-07-05 22:11:55 +0100

    springinaction-ch05-init with in memory user manager of spring security - change to others later

---
+++ springinaction/ch05-basic/src/main/java/com/example/ch05basic/Ch05BasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.ch05basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch05BasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch05BasicApplication.class, args);
+    }
+
+}
---
+++ springinaction/ch05-basic/src/test/java/com/example/ch05basic/Ch05BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ch05basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch05BasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
