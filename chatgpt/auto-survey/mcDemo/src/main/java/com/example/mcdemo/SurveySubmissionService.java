e767052cc8b80db82e69e17650b0b2a8e652d6a6
nanshaqundao <nanshax@hotmail.co.uk>
2023-04-02 18:22:27 +0100

    hi

--- chatgpt/auto-survey/mcDemo/src/main/java/com/example/mcdemo/SurveySubmissionService.java
+++ chatgpt/auto-survey/mcDemo/src/main/java/com/example/mcdemo/SurveySubmissionService.java
@@ -5,11 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.stereotype.Service;
-
-import java.time.Duration;
 
 @Service
 public class SurveySubmissionService {
