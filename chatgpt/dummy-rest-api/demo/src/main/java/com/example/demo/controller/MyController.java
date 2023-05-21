b50c9aff0d58c34fec6f70d2729cc3d02b731851
nanshaqundao <nanshax@hotmail.co.uk>
2023-05-21 20:00:51 +0100

    dummy api for test

--- chatgpt/dummy-rest-api/demo/src/main/java/com/example/demo/controller/MyController.java
+++ chatgpt/dummy-rest-api/demo/src/main/java/com/example/demo/controller/MyController.java
@@ -1,2 +1,28 @@
-package com.example.demo.controller;public class MyController {
+package com.example.demo.controller;
+
+import com.example.demo.model.RefResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class MyController {
+    @GetMapping("/refData")
+    public String getRefData(String srcCode) {
+        return "refData";
+    }
+
+    @GetMapping("/annaData")
+    public ResponseEntity<RefResponse> getAnnaData(String srcCode) {
+        RefResponse refResponse = new RefResponse();
+        refResponse.setSrcCode(srcCode);
+        refResponse.setAnnaCode("annaCode" + srcCode);
+        refResponse.setCrossCode("crossCode" + srcCode);
+        return ResponseEntity.ok(refResponse);
+    }
+
+    @PostMapping("/isin")
+    public String getIsin(@RequestBody String requestBody) {
+        return "isin-" + requestBody;
+    }
 }
--- chatgpt/dummy-rest-api/demo/src/main/java/com/example/demo/model/RefResponse.java
+++ chatgpt/dummy-rest-api/demo/src/main/java/com/example/demo/model/RefResponse.java
@@ -1,2 +1,31 @@
-package com.example.demo.model;public class RefResponse {
+package com.example.demo.model;
+
+public class RefResponse {
+    private String srcCode;
+    private String annaCode;
+    private String crossCode;
+
+    public String getCrossCode() {
+        return crossCode;
+    }
+
+    public void setCrossCode(String crossCode) {
+        this.crossCode = crossCode;
+    }
+
+    public String getSrcCode() {
+        return srcCode;
+    }
+
+    public void setSrcCode(String srcCode) {
+        this.srcCode = srcCode;
+    }
+
+    public String getAnnaCode() {
+        return annaCode;
+    }
+
+    public void setAnnaCode(String annaCode) {
+        this.annaCode = annaCode;
+    }
 }
