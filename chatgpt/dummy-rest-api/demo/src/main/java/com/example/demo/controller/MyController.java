8225977f0f27b3e25254cd9694e87b56687cf0b2
nanshaqundao <nanshax@hotmail.co.uk>
2023-06-16 21:35:22 +0100

    format

--- chatgpt/dummy-rest-api/demo/src/main/java/com/example/demo/controller/MyController.java
+++ chatgpt/dummy-rest-api/demo/src/main/java/com/example/demo/controller/MyController.java
@@ -10,45 +10,45 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api")
 public class MyController {
-    @GetMapping("/refData")
-    public String getRefData(@RequestParam String srcCode) {
-        System.out.println("Request received with details: " + srcCode);
-        return "refData";
+  @GetMapping("/refData")
+  public String getRefData(@RequestParam String srcCode) {
+    System.out.println("Request received with details: " + srcCode);
+    return "refData";
+  }
+
+  @GetMapping("/annaData")
+  public Mono<ResponseEntity<RefResponse>> getAnnaData(@RequestParam String srcCode) {
+    System.out.println("Request received with details: " + srcCode);
+    RefResponse refResponse = new RefResponse();
+    refResponse.setSrcCode(srcCode);
+    refResponse.setAnnaCode("annaCode" + srcCode);
+    refResponse.setCrossCode("crossCode" + srcCode);
+    return Mono.just(ResponseEntity.ok(refResponse));
+  }
+
+  @PostMapping("/isin")
+  public Mono<ResponseEntity<String>> getIsin(@RequestBody String requestBody) {
+    String result = "ISIN-" + generateRandomString(10);
+    System.out.println("Request received with details: " + requestBody);
+    // return Mono.just(ResponseEntity.ok(result));
+
+    return Mono.just(ResponseEntity.internalServerError().body("Error" + result));
+  }
+
+  private String generateRandomString(int length) {
+    StringBuilder sb = new StringBuilder();
+    String uuid = UUID.randomUUID().toString().replace("-", "");
+
+    // Ensure the string starts with a non-digit character
+    char firstChar = (char) (Character.toUpperCase(uuid.charAt(0)) % 26 + 'A');
+    sb.append(firstChar);
+
+    // Append the remaining characters from the UUID string
+    for (int i = 1; i < length; i++) {
+      char randomChar = (char) Character.toUpperCase(uuid.charAt(i) % 26 + 'A');
+      sb.append(randomChar);
     }
 
-    @GetMapping("/annaData")
-    public Mono<ResponseEntity<RefResponse>> getAnnaData(@RequestParam String srcCode) {
-        System.out.println("Request received with details: " + srcCode);
-        RefResponse refResponse = new RefResponse();
-        refResponse.setSrcCode(srcCode);
-        refResponse.setAnnaCode("annaCode" + srcCode);
-        refResponse.setCrossCode("crossCode" + srcCode);
-        return Mono.just(ResponseEntity.ok(refResponse));
-    }
-
-    @PostMapping("/isin")
-    public Mono<ResponseEntity<String>> getIsin(@RequestBody String requestBody) {
-        String result = "ISIN-" + generateRandomString(10);
-        System.out.println("Request received with details: " + requestBody);
-        //return Mono.just(ResponseEntity.ok(result));
-
-        return Mono.just(ResponseEntity.internalServerError().body("Error" + result));
-    }
-
-    private String generateRandomString(int length) {
-        StringBuilder sb = new StringBuilder();
-        String uuid = UUID.randomUUID().toString().replace("-", "");
-
-        // Ensure the string starts with a non-digit character
-        char firstChar = (char) (Character.toUpperCase(uuid.charAt(0)) % 26 + 'A');
-        sb.append(firstChar);
-
-        // Append the remaining characters from the UUID string
-        for (int i = 1; i < length; i++) {
-            char randomChar = (char) Character.toUpperCase(uuid.charAt(i) % 26 + 'A');
-            sb.append(randomChar);
-        }
-
-        return sb.toString();
-    }
+    return sb.toString();
+  }
 }
--- chatgpt/dummy-rest-caller/demo/src/main/java/com/example/demo/service/DummyService.java
+++ chatgpt/dummy-rest-caller/demo/src/main/java/com/example/demo/service/DummyService.java
@@ -1,6 +1,5 @@
 package com.example.demo.service;
 
-
 import com.example.demo.exception.ClientException;
 import com.example.demo.exception.OtherException;
 import com.example.demo.exception.ServerException;
@@ -13,65 +12,78 @@ import reactor.util.retry.RetryBackoffSpec;
 
 import java.time.Duration;
 
-
 @Service
 public class DummyService {
 
-    private final WebClient webClientWithTimeout;
+  private final WebClient webClientWithTimeout;
 
-    public DummyService(WebClient webClientWithTimeout) {
-        this.webClientWithTimeout = webClientWithTimeout;
+  public DummyService(WebClient webClientWithTimeout) {
+    this.webClientWithTimeout = webClientWithTimeout;
 
-        System.out.println("DummyService: " + webClientWithTimeout);
-    }
+    System.out.println("DummyService: " + webClientWithTimeout);
+  }
 
-//    public Mono<String> getIsin() {
-//        ///api/isin
+  //    public Mono<String> getIsin() {
+  //        ///api/isin
 
-    //        var monoX = webClientWithTimeout.post()
-//                .uri(uriBuilder -> uriBuilder.path("/api/isin").build())
-//                .body(BodyInserters.fromValue("dummy"))
-//                .retrieve()
-//
-//                .onStatus(HttpStatusCode::is4xxClientError, clientResponse -> {
-//                    System.out.println("4xx");
-//                    return Mono.error(new ClientException("Client error"));
-//                })
-//                .onStatus(HttpStatusCode::is5xxServerError, clientResponse -> {
-//                    System.out.println("5xx");
-//                    return Mono.error(new ServerException("Server error"));
-//                })
-//                .bodyToMono(String.class)
-//                .timeout(Duration.ofSeconds(5))
-//                .doOnError(throwable -> System.out.println("doOnError: " + throwable))
-//                .retryWhen(Retry.backoff(3, Duration.ofSeconds(3)).doBeforeRetry(System.out::println).filter(throwable -> !(throwable instanceof ClientException || throwable instanceof ServerException)));
-    public Mono<String> getIsin() {
-        var monoX = webClientWithTimeout.post().uri(uriBuilder -> uriBuilder.path("/api/isin").build()).body(BodyInserters.fromValue("dummy")).exchangeToMono(clientResponse -> {
-            if (clientResponse.statusCode().is4xxClientError()) {
-                System.out.println("4xx is not considered an error");
-                return clientResponse.bodyToMono(String.class);
-            } else if (clientResponse.statusCode().is5xxServerError()) {
-                System.out.println("5xx");
-                return clientResponse.bodyToMono(String.class).flatMap(body -> Mono.error(new ServerException("Server error: " + body)));
-            } else {
-                return clientResponse.bodyToMono(String.class);
-            }
-        }).doOnNext(result -> {
-            System.out.println("doOnNext: " + result);
+  //        var monoX = webClientWithTimeout.post()
+  //                .uri(uriBuilder -> uriBuilder.path("/api/isin").build())
+  //                .body(BodyInserters.fromValue("dummy"))
+  //                .retrieve()
+  //
+  //                .onStatus(HttpStatusCode::is4xxClientError, clientResponse -> {
+  //                    System.out.println("4xx");
+  //                    return Mono.error(new ClientException("Client error"));
+  //                })
+  //                .onStatus(HttpStatusCode::is5xxServerError, clientResponse -> {
+  //                    System.out.println("5xx");
+  //                    return Mono.error(new ServerException("Server error"));
+  //                })
+  //                .bodyToMono(String.class)
+  //                .timeout(Duration.ofSeconds(5))
+  //                .doOnError(throwable -> System.out.println("doOnError: " + throwable))
+  //                .retryWhen(Retry.backoff(3,
+  // Duration.ofSeconds(3)).doBeforeRetry(System.out::println).filter(throwable -> !(throwable
+  // instanceof ClientException || throwable instanceof ServerException)));
+  public Mono<String> getIsin() {
+    var monoX =
+        webClientWithTimeout
+            .post()
+            .uri(uriBuilder -> uriBuilder.path("/api/isin").build())
+            .body(BodyInserters.fromValue("dummy"))
+            .exchangeToMono(
+                clientResponse -> {
+                  if (clientResponse.statusCode().is4xxClientError()) {
+                    System.out.println("4xx is not considered an error");
+                    return clientResponse.bodyToMono(String.class);
+                  } else if (clientResponse.statusCode().is5xxServerError()) {
+                    System.out.println("5xx");
+                    return clientResponse
+                        .bodyToMono(String.class)
+                        .flatMap(body -> Mono.error(new ServerException("Server error: " + body)));
+                  } else {
+                    return clientResponse.bodyToMono(String.class);
+                  }
+                })
+            .doOnNext(
+                result -> {
+                  System.out.println("doOnNext: " + result);
+                })
+            .doOnError(
+                error -> {
+                  System.out.println("doOnError: " + error);
+                })
+            .retryWhen(retrySpec);
+    return monoX;
+  }
 
-        }).doOnError(error -> {
-            System.out.println("doOnError: " + error);
-
-        }).retryWhen(retrySpec);
-        return monoX;
-    }
-
-    private RetryBackoffSpec retrySpec =
-            Retry.backoff(3, Duration.ofSeconds(3))
-                    .jitter(0.5)
-                    .doBeforeRetry(info -> System.out.println("doBeforeRetry: " + info.totalRetriesInARow()))
-                    .filter(
-                            throwable -> !(throwable instanceof ClientException || throwable instanceof ServerException))
-                    .onRetryExhaustedThrow(
-                            (retryBackoffSpec, retrySignal) -> new OtherException("Retry exhausted"));
+  private RetryBackoffSpec retrySpec =
+      Retry.backoff(3, Duration.ofSeconds(3))
+          .jitter(0.5)
+          .doBeforeRetry(info -> System.out.println("doBeforeRetry: " + info.totalRetriesInARow()))
+          .filter(
+              throwable ->
+                  !(throwable instanceof ClientException || throwable instanceof ServerException))
+          .onRetryExhaustedThrow(
+              (retryBackoffSpec, retrySignal) -> new OtherException("Retry exhausted"));
 }
