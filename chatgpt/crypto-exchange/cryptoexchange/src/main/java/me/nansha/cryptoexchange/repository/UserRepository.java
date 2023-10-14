55bc9b719f4f53531f0cc0e940b79be6504c0857
nanshaqundao <nanshax@hotmail.co.uk>
2023-10-14 22:50:13 +0100

    stage 1

---
+++ chatgpt/crypto-exchange/cryptoexchange/src/main/java/me/nansha/cryptoexchange/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package me.nansha.cryptoexchange.repository;
+
+import me.nansha.cryptoexchange.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+  Optional<User> findByUsername(final String username);
+}
