dc7baeadb012f7ecdc55a77d090b6bdafbf42b02
nanshaqundao <nanshax@hotmail.co.uk>
2024-01-28 01:02:55 +0000

    use ResolvableTypeProvider interface to avoid generic type swipe

--- eventlistener/event-listener/src/main/java/org/example/eventlistener/listener/EventListenerService.java
+++ eventlistener/event-listener/src/main/java/org/example/eventlistener/listener/EventListenerService.java
@@ -6,25 +6,35 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class EventListenerService {
+//    @EventListener
+//    public void handlePersonEvent(PersonEvent personEvent) {
+//        System.out.println("Get PersonEvent: " + personEvent);
+//    }
+//
+//    @EventListener
+//    public void handleOrderEvent(OrderEvent orderEvent) {
+//        System.out.println("Get OrderEvent: " + orderEvent);
+//    }
+//
+//    @EventListener
+//    public void handleEvent(BaseEvent<?> baseEvent) {
+//        System.out.println("Get BaseEvent: " + baseEvent);
+//        Object data = baseEvent.data();
+//        if (data instanceof Person) {
+//            System.out.println("Extracted PersonEvent: " + data);
+//        } else if (data instanceof Order) {
+//            System.out.println("Extracted OrderEvent: " + data);
+//        }
+//
+//    }
+
     @EventListener
-    public void handlePersonEvent(PersonEvent personEvent) {
+    public void handlePersonEvent(BaseEvent<Person> personEvent) {
         System.out.println("Get PersonEvent: " + personEvent);
     }
 
     @EventListener
-    public void handleOrderEvent(OrderEvent orderEvent) {
+    public void handleOrderEvent(BaseEvent<Order> orderEvent) {
         System.out.println("Get OrderEvent: " + orderEvent);
-    }
-
-    @EventListener
-    public void handleEvent(BaseEvent<?> baseEvent) {
-        System.out.println("Get BaseEvent: " + baseEvent);
-        Object data = baseEvent.data();
-        if (data instanceof Person) {
-            System.out.println("Extracted PersonEvent: " + data);
-        } else if (data instanceof Order) {
-            System.out.println("Extracted OrderEvent: " + data);
-        }
-
     }
 }
--- eventlistener/event-listener/src/main/java/org/example/eventlistener/model/BaseEvent.java
+++ eventlistener/event-listener/src/main/java/org/example/eventlistener/model/BaseEvent.java
@@ -1,4 +1,11 @@
 package org.example.eventlistener.model;
 
-public record BaseEvent<T>(T data, String addOrUpdate) {
+import org.springframework.core.ResolvableType;
+import org.springframework.core.ResolvableTypeProvider;
+
+public record BaseEvent<T>(T data, String addOrUpdate) implements ResolvableTypeProvider {
+    @Override
+    public ResolvableType getResolvableType() {
+        return ResolvableType.forClassWithGenerics(getClass(), ResolvableType.forInstance(data));
+    }
 }
