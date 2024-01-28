36d0d1a14815f22823f7d72fdcb2a39f64507cd4
nanshaqundao <nanshax@hotmail.co.uk>
2024-01-28 10:45:30 +0000

    refactor

--- eventlistener/event-listener/src/main/java/org/example/eventlistener/rest/TestController.java
+++ eventlistener/event-listener/src/main/java/org/example/eventlistener/rest/TestController.java
@@ -1,10 +1,7 @@
 package org.example.eventlistener.rest;
 
 import jakarta.annotation.Resource;
-import org.example.eventlistener.model.Order;
-import org.example.eventlistener.model.OrderEvent;
-import org.example.eventlistener.model.Person;
-import org.example.eventlistener.model.PersonEvent;
+import org.example.eventlistener.model.*;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +20,11 @@ public class TestController {
     @GetMapping("/publishAddOrderEvent")
     public void publishAddOrderEvent() {
         applicationContext.publishEvent(new OrderEvent(new Order("Order 1"), "add"));
+    }
+
+    @GetMapping("/publishAddEvent")
+    public void publishEvent() {
+        applicationContext.publishEvent(new BaseEvent<>(new Person("John"), "add"));
+        applicationContext.publishEvent(new BaseEvent<>(new Order("John's Order"), "update"));
     }
 }
