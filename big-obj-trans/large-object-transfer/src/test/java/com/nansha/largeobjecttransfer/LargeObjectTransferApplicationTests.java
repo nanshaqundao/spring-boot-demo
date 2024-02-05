15c3bd644e2a14d10c61c31fdd6b4db917a4e16e
nanshaqundao <nanshax@hotmail.co.uk>
2024-02-05 09:39:00 +0000

    new project

---
+++ big-obj-trans/large-object-transfer/src/test/java/com/nansha/largeobjecttransfer/LargeObjectTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nansha.largeobjecttransfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LargeObjectTransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
