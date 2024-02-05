a7133d953461ce1fd307deb839a2ea15d95d62be
nanshaqundao <nanshax@hotmail.co.uk>
2024-02-05 16:05:24 +0000

    update

---
+++ big-obj-trans/large-object-transfer-client/larg-object-trans-client/src/main/java/com/nansha/largobjecttransclient/LargObjectTransClientApplication.java
@@ -0,0 +1,13 @@
+package com.nansha.largobjecttransclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LargObjectTransClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LargObjectTransClientApplication.class, args);
+    }
+
+}
---
+++ big-obj-trans/large-object-transfer/src/main/java/com/nansha/largeobjecttransfer/service/MessageService.java
@@ -0,0 +1,2 @@
+package com.nansha.largeobjecttransfer;public class MessageService {
+}
