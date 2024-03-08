b3abbf5e5d6283f75e2bf42e7ced9ae0031e763f
nanshaqundao <nanshax@hotmail.co.uk>
2024-03-08 23:02:26 +0000

    fix test

--- big-obj-trans/large-object-transfer-client/larg-object-trans-client/src/test/java/com/nansha/largobjecttransclient/util/CsvUtilsTest.java
+++ big-obj-trans/large-object-transfer-client/larg-object-trans-client/src/test/java/com/nansha/largobjecttransclient/util/CsvUtilsTest.java
@@ -7,8 +7,6 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class CsvUtilsTest {
     @Test
     void testConvertListToCsv_WithValidInput_ShouldReturnCorrectCsvString() {
